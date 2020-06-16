9d312d5d2db6f42f9bd62e3ff4a64018e770cd08
Reino <reino7@tlu.ee>
2020-06-16 07:10:49 +0300

    created tab Rating

---
+++ src/app/rating/rating.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: 'rating.page.html',
+  styleUrls: ['rating.page.scss']
+})
+export class RatingPage {
+
+  constructor() {}
+
+}
