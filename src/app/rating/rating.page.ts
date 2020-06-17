fa9b47fe3cd1c988920e59ec23bf00afddd97125
Reino <reino7@tlu.ee>
2020-06-17 12:43:01 +0300

    inserted Rating dummy info

--- src/app/rating/rating.page.ts
+++ src/app/rating/rating.page.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class RatingPage {
 
-  constructor() {}
+  public ratings: Array<{ rateorder: number, firstname: string, famname: string}> = [];
+
+  constructor() {
+
+    this.ratings.push(
+      { rateorder: 1, firstname: 'VALLOT', famname: 'VAINULA' },
+      { rateorder: 2, firstname: 'Narco', famname: 'SMIRNOV' },
+      { rateorder: 3, firstname: 'Bombasto', famname: 'LUŠIN' },
+      { rateorder: 4, firstname: 'Celeritas', famname: 'STROGOV' },
+      { rateorder: 5, firstname: 'Magneta', famname: 'LAANE' },
+      { rateorder: 6, firstname: 'RubberMan', famname: 'LAURI' },
+      { rateorder: 7, firstname: 'Dynama', famname: 'STANISLAV' },
+      { rateorder: 8, firstname: 'Dr IQ', famname: 'ALEKSANDR' },
+      { rateorder: 9, firstname: 'Magma', famname: 'VALLOT' },
+      { rateorder: 10, firstname: 'Tornado', famname: 'ALEKSANDR' }    
+    );
+  }
+
 
 }
